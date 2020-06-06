9e74abe220d377082f8fe95d68ab913b5989b1fa
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 23:05:25 +0500

    Add files via upload

---
+++ ForgetPassword.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Serving_Hands
+{
+    public partial class ForgetPassword : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Models.ForgetPassword fp = new Models.ForgetPassword();
+            fp.Email = TextBox1.Text;
+            bool st = fp.SearchEmail(fp);
+            if(st == true)
+            {
+                Response.Redirect("PasswordRecovery.aspx");
+            }
+            else
+            {
+                Response.Write("No Email Found");
+            }
+        }
+    }
+}

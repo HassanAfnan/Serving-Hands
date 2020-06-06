4ead45879944877d45a88ac72d31a23bd53f0736
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 22:08:18 +0500

    Add files via upload

---
+++ AdminPage.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Serving_Hands
+{
+    public partial class AdminPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Models.Login lo = Session["UserData"] as Models.Login;
+            Label1.Text = lo.Email;
+        }
+    }
+}

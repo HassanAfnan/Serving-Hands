0227b19a65581375b8211fe1ea2e03b65ec3bd1d
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:33:25 +0500

    Add files via upload

---
+++ Models/ClearTextBox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Serving_Hands.Models
+{
+    public class ClearTextBox
+    {
+        public void ClearTextbox(params TextBox[] textBoxes)
+        {
+            for (int i = 0; i < textBoxes.Length; i++)
+            {
+                textBoxes[i].Text = "";
+            }
+        }
+    }
+}

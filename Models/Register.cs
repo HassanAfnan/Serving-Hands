0cff61d5b26d46b972bc3d7ce7e7cc4c0675e30c
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:38:44 +0500

    Add files via upload

---
+++ Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Serving_Hands.Models
+{
+    public class Register
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Pass { get; set; }
+        public string gender { get; set; }
+        public string contact { get; set; }
+        public string address { get; set; }
+        public string date { get; set; }
+
+
+
+    }
+}

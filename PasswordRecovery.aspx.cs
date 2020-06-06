3151d9c74b6dcc117f7c4fe895e06434c8da27a6
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 23:16:41 +0500

    Add files via upload

---
+++ PasswordRecovery.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Serving_Hands
+{
+    public partial class PasswordRecovery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Models.Email em = new Models.Email();
+            em.email = TextBox1.Text;
+            em.SendEmail(em);
+            Response.Write("Email sent");
+        }
+    }
+}

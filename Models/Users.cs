916c34b097b41e546455c2d5e5e7451702e351f9
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:39:11 +0500

    Add files via upload

---
+++ Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Serving_Hands.Models
+{
+    public class Users : Register
+    {
+        public string role = "User";
+        public void Signup(Users u)
+        {
+            Connection con = new Connection();
+            con.GetConnection();
+            con.ExecuteQuery("insert into Register values('" + u.Name + "','" + u.Email + "','" + u.Pass + "','" + u.gender + "','" + u.contact + "','" + u.address + "','" + u.date + "','" + u.role + "')");
+        }
+    }
+}

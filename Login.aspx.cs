b739e18de1fb6c57f5116732f026dce7f2167e59
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 23:14:38 +0500

    Add files via upload

---
+++ Login.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Serving_Hands
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+             Models.Login log = new Models.Login();
+            log.Email = TextBox1.Text;
+            log.Password = TextBox2.Text;
+            log.GetLogin(log);
+            if(log.cond == true && log.role == "Admin")
+            {
+                Response.Write("<script>alert('Welcome Admin')</script>");
+                log.Email = TextBox1.Text;
+                log.Password = TextBox2.Text;
+                Session["UserData"] = log;
+                Response.Redirect("AdminPage.aspx");
+            }
+            else if (log.cond == true && log.role == "User")
+            {
+                Response.Write("<script>alert('Welcome User')</script>");
+                log.Email = TextBox1.Text;
+                log.Password = TextBox2.Text;
+                Session["UserData"] = log;
+                Response.Redirect("UserPage.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('Invalid User Id Or Password')</script>");
+            }
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ForgetPassword.aspx");
+        }
+    }
+}

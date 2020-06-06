5ecf0e74ac12fd1365fce6cba82314af03d5ce2b
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:35:06 +0500

    Add files via upload

---
+++ Models/Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Serving_Hands.Models
+{
+    public class Connection
+    {
+        public SqlConnection con = new SqlConnection();
+        public SqlConnection GetConnection()
+        {
+            if(con.ConnectionString == "")
+            {
+                con = new SqlConnection("Data Source=DESKTOP-FVIQF0N;Initial Catalog=ServingHands;Integrated Security=True");
+                con.Open();
+            }
+            return con;
+        }
+        public void ExecuteQuery(string query)
+        {
+            SqlCommand com = new SqlCommand(query,con);
+            com.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}

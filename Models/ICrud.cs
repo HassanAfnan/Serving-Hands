d8267d6fb9b286f9cf12fc20923afc3a378e7625
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:36:51 +0500

    Add files via upload

---
+++ Models/ICrud.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serving_Hands.Models
+{
+    interface ICrud<T>
+    {
+        void Add(T s);
+        void Update(T s);
+        void Delete(T s);
+        T Search(T s);
+        List<T> GetIds();
+        List<T> GetOrganization();
+        StringReader Recipt(T a);
+    }
+}

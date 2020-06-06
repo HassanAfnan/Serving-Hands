125ca48df6e4321b532c17c0a3bd5d9b8b2ae63d
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-06 21:31:40 +0500

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace Serving_Hands.Models
+{
+    public class Admin : Register
+    {
+        public string role = "Admin";
+        public void Signup(Admin a)
+        {
+            Connection con = new Connection();
+            con.GetConnection();
+            con.ExecuteQuery("insert into Register values('"+a.Name+"','"+a.Email+"','"+a.Pass+"','"+a.gender+"','"+a.contact+"','"+a.address+"','"+a.date+"','"+a.role+"')");
+        }
+    }
+}
